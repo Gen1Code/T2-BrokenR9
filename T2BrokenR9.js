dcfe0db94035c0359b01684b0ad4f0542d32a662
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2022-11-30 01:26:37 +0000

    added sigma effect

--- T2BrokenR9.js
+++ T2BrokenR9.js
@@ -228,7 +228,7 @@ var getQuaternaryEntries = () => {
     return quaternaryEntries;
 }
 
-var getPublicationMultiplier = (tau) => tau.pow(0.198) / BigNumber.HUNDRED;
+var getPublicationMultiplier = (tau) => tau.pow(0.198)*(BigNumber.TEN*BigNumber.THREE/(BigNumber.TWO)).pow(3)/ BigNumber.HUNDRED;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.198}}{100}";
 var getTau = () => currency.value;
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
